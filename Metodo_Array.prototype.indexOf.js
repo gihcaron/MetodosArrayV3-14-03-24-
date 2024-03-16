c8fd259a6b46378d91fb9a876a0d7ff05ffdeb43
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 19:53:06 -0300

    Metodo _Array.prototype.indexOf.js - Exemplo 2 + alterações nos textos de apoio

--- Metodo_Array.prototype.indexOf.js
+++ Metodo_Array.prototype.indexOf.js
@@ -1,6 +1,6 @@
 // Metodo 3: Metodo_Array.prototype.indexOf
 
-//Utilizadade:
+//Utilidade: O método Array.prototype.indexOf é utilizado para encontrar o indíce da primeira ocorrência de um elemento determinado em uma Array
 
 //Sitaxe: indexOf(searchElement)
 // indexOf(searchElement, fromIndex)
@@ -8,10 +8,22 @@
 // Exemplo 1 ----------------------------
 
 //Declarar a constante Escuderias de Formula 1
-const EscuderiasF1 = ('Mercedes', 'Ferrari', 'McLaren', "RedBull", "Haas");
+//const EscuderiasF1 = ('Mercedes', 'Ferrari', 'McLaren', "RedBull", "Haas");
 
 // Exibir se a escuderia "Williams" se encontrea na lista
-console.log(EscuderiasF1.indexOf("Williams"));
+//console.log(EscuderiasF1.indexOf("Williams"));
 
 //Saida Esperada: -1
-//Justificativa: A Escuderia procurada existe na lista, logo sua posição é negativa. 
+//Justificativa: A Escuderia procurada existe na lista, logo sua posição é negativa, ou seja, não determinada numa Array. 
+
+//Exemplo 2 ------------------------------------------
+//Declarar a frase desejada
+let frase = 'Longe se vai sonhando demais';
+
+//Procurar uma string ("Vai") dentro de outra (frase)
+//Exibir a string encontrada e sua posição
+console.log(frase.indexOf("vai"));
+
+//Saída esperada: 9
+//Justificativa: O 9 é onde foi mencionada a palavra "Vai" pela primeira vez na Array (Frase)
+
