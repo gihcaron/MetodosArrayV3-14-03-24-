c228bea5e2fdb148b8fbf69ec666fd00bcdc51ac
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 20:12:36 -0300

    Metodo _Array.isArray - Exemplo 1

---
+++ Metodo_Array.isArray.js
@@ -0,0 +1,18 @@
+// Método 4: Array.isArray()
+
+// Utilidade: O Array.isArray() é utilizado para verificar se o valor desejado/mencionado é uma Array ou não. 
+
+//Sitaxe: Array.isArray(value)
+
+//Exemplo 1-------------------------------------
+
+//Insira ou determine o valor desejado
+
+let numbers = 1 ;
+
+//Exiba se o Array realmente é um Array ou não 
+console.log (Array.isArray(numbers));
+
+//Resultado esperado: False 
+
+// Exemplo 2 ------------------------------------
