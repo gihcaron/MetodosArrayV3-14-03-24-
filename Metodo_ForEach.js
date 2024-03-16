d4d9f748ee5c02df370ee4e2cdbdcf994669bbb7
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 17:04:59 -0300

    Metodo ForEach - Exemplo 2

--- Metodo_ForEach.js
+++ Metodo_ForEach.js
@@ -17,3 +17,29 @@ const numbers = [4, 8, 28, 32];
   console.log(" O Numero é " + numbers + " e está na posição " + index);
 
 });
+
+//Exemplo 2 - ForEach: Como posso aplicar o ForEach na prática?
+// Tenho uma lista de alunos e quero saber somente quem são os maiores de idade 
+
+// Declarar a lista de alunos
+
+const alunos = [
+    { nome: "Beyonce", idade: "15", turma: "1TDS1" },
+    { nome: "Luan Santana", idade: "18", turma: "M5B" },
+    { nome: "Lady Gaga", idade: "19", turma: "3TEE" },
+    { nome: "Justi Biber", idade: "17", turma: "2TAD" },
+
+]
+
+//Para cada aluno 
+// Verificar se ele é maior de idade
+//Se for, exibir "O aluno, x  posição index da turma x é maior de idade" 
+
+alunos.forEach(function (item, index){
+    if(item.idade >= 18){
+    console.log('O aluno(a), ' + item.nome + '  posição ' + index + ' da turma ' + item.turma + ' é maior de idade')
+    
+}
+});
+
+
