d6d8f19ff2df236c0f90e406171bf814ae458101
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 20:56:54 -0300

    Metodo_Array.prototype.join_ Exemplo 2 - V2

--- Metodo_Array.prototype.join.js
+++ Metodo_Array.prototype.join.js
@@ -11,20 +11,22 @@ const names = ["Maria Flor", "Raissa", "Theo", "Carla"];
 
 // Determinar de qual forma ela irão se "unir"
 // Forma definida = Através de ","
-// Exibir os nomes agrupados através com as ","
+// Exibir os nomes agrupados  com as ","
 
 console.log(names.join());
 
 // Saída esperada: Maria Flor,Raissa,Theo,Carla
 
 //Exemplo 2 -------------------------------
+
 // Declare a array desejada 
 
-let letras = ['G', 'I', 'O', 'V', 'A', 'N', 'N', 'A']; 
+let letras = ['G', 'I', 'O', 'V', 'A', 'N', 'N', 'A'];
 
 // Determinar de qual forma ela irão se "unir"
+// Méotodo decidido -  As letras irão se unir através de "-""
 // // Exibir as letras agrupadas
 
-console.log(letras.join(''));
+console.log(letras.join('-'));
 
 // Saída esperada: "GIOVANNA"
