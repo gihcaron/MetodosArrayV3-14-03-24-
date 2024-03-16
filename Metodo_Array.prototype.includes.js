4150382e6a47788d37e0181309737723e046170a
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 17:41:21 -0300

    Metodo _Array.prototype.includes - Exemplo 1

---
+++ Metodo_Array.prototype.includes.js
@@ -0,0 +1,20 @@
+// Metodo 2: Array.prototype.includes()
+
+// Utilidade: É utilizado quando for preciso saber se o array ou objeto possui determinado valor ou elemento
+
+// Sintaxe: includes(searchElement)
+//includes(searchElement, fromIndex)
+
+// Exemplo 1 
+
+// Determinar constante de cores 
+const cores = ['Amarelo', 'Azul', 'Verde', 'Rosa'];
+
+// Exibir e determinar (se é falso ou verdadeiro) se inclue a cor "Ciano" na lista 
+console.log(cores.includes("Ciano"));
+
+//Saida Esperada: Falso
+
+
+
+
