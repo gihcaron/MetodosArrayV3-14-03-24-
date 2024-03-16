5812b689ee04994e37611016877645b68b4e3774
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 20:41:42 -0300

    Metodo_Array.prototype.join_ Exemplo 1

---
+++ Metodo_Array.prototype.join.js
@@ -0,0 +1,20 @@
+// Metodo 5 : Array.prototype.join()
+
+// Utilidade: Cria uma nova String de uma Array cancatenando todos os elementos dessa mesma Array.  
+
+// Sitaxe: Array.prototype.join()
+
+// Exemplo 1 -------------------------------
+
+// Declare a array desejada a cancatenar
+const names = ["Maria Flor", "Raissa", "Theo", "Carla"];
+
+// Determinar de qual forma ela irão se "unir"
+// Forma definida = Através de ","
+// Exibir os nomes agrupados através com as ","
+
+console.log(names.join());
+
+// Saída esperada: Maria Flor,Raissa,Theo,Carla
+
+//Exemplo 2 -------------------------------
