f4a49cb622f47732660c0ce0065288641cf69acc
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 19:21:54 -0300

    Metodo _Array.prototype.indexOf.js - Exemplo 1

---
+++ Metodo_Array.prototype.indexOf.js
@@ -0,0 +1,17 @@
+// Metodo 3: Metodo_Array.prototype.indexOf
+
+//Utilizadade:
+
+//Sitaxe: indexOf(searchElement)
+// indexOf(searchElement, fromIndex)
+
+// Exemplo 1 ----------------------------
+
+//Declarar a constante Escuderias de Formula 1
+const EscuderiasF1 = ('Mercedes', 'Ferrari', 'McLaren', "RedBull", "Haas");
+
+// Exibir se a escuderia "Williams" se encontrea na lista
+console.log(EscuderiasF1.indexOf("Williams"));
+
+//Saida Esperada: -1
+//Justificativa: A Escuderia procurada existe na lista, logo sua posição é negativa. 
