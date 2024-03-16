055deea4291a49d5221f83a68fbf55be7b3f7473
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 18:05:35 -0300

    Metodo _Array.prototype.includes - Exemplo 2

--- Metodo_Array.prototype.includes.js
+++ Metodo_Array.prototype.includes.js
@@ -2,10 +2,10 @@
 
 // Utilidade: É utilizado quando for preciso saber se o array ou objeto possui determinado valor ou elemento
 
-// Sintaxe: includes(searchElement)
-//includes(searchElement, fromIndex)
+// Sintaxe: includes(searchElement);
+//includes(searchElement, fromIndex);
 
-// Exemplo 1 
+// Exemplo 1 ------------------------------------------
 
 // Determinar constante de cores 
 const cores = ['Amarelo', 'Azul', 'Verde', 'Rosa'];
@@ -13,8 +13,23 @@ const cores = ['Amarelo', 'Azul', 'Verde', 'Rosa'];
 // Exibir e determinar (se é falso ou verdadeiro) se inclue a cor "Ciano" na lista 
 console.log(cores.includes("Ciano"));
 
-//Saida Esperada: Falso
+//Saída Esperada: Falso
 
+// Exemplo 2 -------------------------------------
 
+// Determinar e declarar a constate alunos 
+const alunos = [
+    { nome: "Beyonce", idade: "15", turma: "1TDS1" },
+    { nome: "Luan Santana", idade: "18", turma: "M5B" },
+    { nome: "Lady Gaga", idade: "19", turma: "3TEE" },
+    { nome: "Justi Biber", idade: "17", turma: "2TAD" },
+    { nome: "Elon Musk", idade: "20", turma: "2TDS1" },
+]
 
+// Criar uma constate ("filteredUsers") que receberia essas pessoas filtrando se elas incluem um "on" em seu nome
+//Exibir os nomes com "on" 
+const filteredUsers = alunos.filter(a => a.nome.includes("on"));
+console.log(filteredUsers);
 
+// Saída esperada: { nome: 'Beyonce', idade: '15', turma: '1TDS1' },
+// { nome: 'Elon Musk', idade: '20', turma: '2TDS1' },
