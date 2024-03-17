b2ff19a3350a0ccd47892b6ade27bdb4ac55560c
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 11:27:25 -0300

    Metodo_Reverse - Exemplo 1

---
+++ Array.prototype.reverse().js
@@ -0,0 +1,21 @@
+// Método 10: Array.prototype.reverse() 
+
+// Utilidade: inverte um array no lugar. 
+// O primeiro elemento do array agora se tornando o último, e o último elemento do array se tornando o primeiro.
+
+//Sitaxe:reverse()
+
+//Exemplo 1 ---------------------------
+
+let frase = "Amo codar : ) "
+
+//Armaenar em uma outra variável: --
+// O resultado igualar, divir a uma string vazia, aplicar o método reverse ao Array e depois juntar novamente os elementos dentro da String (com o Metodo Join) 
+
+let resultado = frase.split('').reverse().join('');
+
+console.log(resultado); 
+
+
+
+//Exemplo 2 ------------------------------
