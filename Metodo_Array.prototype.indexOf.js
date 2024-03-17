fa831dbea4c02d5e83fe983020136e8da0f12858
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 10:53:39 -0300

    Metodo_Array.pototype.indexOff V2

--- Metodo_Array.prototype.indexOf.js
+++ Metodo_Array.prototype.indexOf.js
@@ -7,11 +7,11 @@
 
 // Exemplo 1 ----------------------------
 
-//Declarar a constante Escuderias de Formula 1
-//const EscuderiasF1 = ('Mercedes', 'Ferrari', 'McLaren', "RedBull", "Haas");
+//Declarar a Array Escuderias de Formula 1
+let EscuderiasF1 = ('Mercedes', 'Ferrari', 'McLaren', "RedBull", "Haas");
 
 // Exibir se a escuderia "Williams" se encontrea na lista
-//console.log(EscuderiasF1.indexOf("Williams"));
+console.log(EscuderiasF1.indexOf("Williams"));
 
 //Saida Esperada: -1
 //Justificativa: A Escuderia procurada existe na lista, logo sua posição é negativa, ou seja, não determinada numa Array. 
