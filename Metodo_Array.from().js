54f5d6d8cd07b2c4e6bf80a648c173aabfe710ee
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 11:09:10 -0300

    Metodo_Array.prototype.from - Exemplo 2

--- Metodo_Array.from().js
+++ Metodo_Array.from().js
@@ -18,6 +18,12 @@ console.log(Array.from(nome));
 // Exemplo 2 ----------------------------------------------
 
 //Declarar uma string de números
-const str = (28042008);
 
-console.log(arry.from(str));
+let numeros = "28042008";
+
+//Exibir os numeros transformados em formato de Array
+
+console.log(Array.from(numeros));
+
+//Saída esperada: ['2', '8', '0', '4', '2', '0', '0', '8']
+
