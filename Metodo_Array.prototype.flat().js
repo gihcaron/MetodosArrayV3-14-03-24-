d2a13b0c0c52e9f612595c1fcf0bda6563d2ac9b
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 12:07:11 -0300

    Metodo_Flat() - Exemplo 1

---
+++ Metodo_Array.prototype.flat().js
@@ -0,0 +1,21 @@
+// Método 11 - Array.prototype.flat()
+
+//Utilidade: O Flat cria uma nova Array com todos os elementos de subArrays concatenados 
+
+//Sintaxe: flat()
+
+//Exemplo 1 -----------------------
+
+//Determinar a Array e suas subarrays
+
+let ListaDeNumeros = [1, [2,  [3, [ 4, [5, [6]]]  ]]  ];
+
+//Igualar o resultado a lista de numeros pelo Flat, o numero 5 determina a profundidade das subArrays que ele deve regatar para criar a nova Array
+
+let resultado = ListaDeNumeros.flat(5);
+
+//Exibir o resultado da linha a cima
+
+console.log(resultado); 
+
+//Saida esperada: [ 1, 2, 3, 4, 5, 6 ]
