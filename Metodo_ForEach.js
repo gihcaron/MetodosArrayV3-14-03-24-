9b49dfd2d06fad11694794290f0bc61308fa160d
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 11:00:49 -0300

    Metodo_ForEach V2 - (Formatação e mudança para variável)

--- Metodo_ForEach.js
+++ Metodo_ForEach.js
@@ -4,31 +4,33 @@
 
 //Sitaxe: array1.forEach((element) => console.log(element));
 
-//Exemplo 1 - ForEach
+//Exemplo 1 - ForEach ----------------------------------------------------
 
 // Declarar os números (Array) desejados
 
-const numbers = [4, 8, 28, 32];
+let numbers = [4, 8, 28, 32];
 
 //Para cada núemro e index
 //Exibir "O Numero é X e está na posição index"
 
 numbers.forEach((numbers, index) => {
     console.log(" O Numero é " + numbers + " e está na posição " + index);
-    
+
 });
 
 //Saida Esperada: O Numero é 4 e está na posição 0
-    //O Numero é 8 e está na posição 1
-    // O Numero é 28 e está na posição 2
-    //O Numero é 32 e está na posição 3
+//O Numero é 8 e está na posição 1
+// O Numero é 28 e está na posição 2
+//O Numero é 32 e está na posição 3
 
+//------------------------------------------------------------------
 //Exemplo 2 - ForEach: Como posso aplicar o ForEach na prática?
+
 // Tenho uma lista de alunos e quero saber somente quem são os maiores de idade 
 
 // Declarar a lista de alunos
 
-const alunos = [
+let alunos = [
     { nome: "Beyonce", idade: "15", turma: "1TDS1" },
     { nome: "Luan Santana", idade: "18", turma: "M5B" },
     { nome: "Lady Gaga", idade: "19", turma: "3TEE" },
@@ -47,7 +49,7 @@ alunos.forEach(function (item, index) {
     }
 });
 
-// Saida Esperada: 
+// Saida Esperada:
 // O aluno(a), Luan Santana  posição 1 da turma M5B é maior de idade
 //O aluno(a), Lady Gaga  posição 2 da turma 3TEE é maior de idade
 
