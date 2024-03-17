705f421a876c35dd0611a63b7c307bb76153fbb0
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 12:53:25 -0300

    Metodo_Filter() - Exemplo 2

--- Metodo_Array.Filter.js
+++ Metodo_Array.Filter.js
@@ -10,15 +10,34 @@
 
 //Inseri alguns nomes aleatórios com algumas repetições propositais
 
-let nomes = [ 'Ana', 'Pedro', 'Paulo', 'Ana', 'Noah', 'Pedro'];
+let nomes = ['Ana', 'Pedro', 'Paulo', 'Ana', 'Noah', 'Pedro'];
 
-// Igualei minha arrei filtrada á:
-// Os nomes pelo filtro receberá a informação se o indice do Array original é o mesmo do indice do elemento 
-// Tendo isso em vista, quando passar pelos nomes e não bater com a igualdade de indices ele não vai filtrar
-//Consequentemente, eliminando os nomes repetidos
+// Igualei minha Array filtrada á:
+// Os nomes pelo filtro que receberá a informação se o indice do Array original é o mesmo do indice do elemento 
+// Tendo isso em vista, quando passar pelos nomes e não bater com a igualdade de indices, ele não vai filtrar
+//Consequentemente, eliminando os nomes repetidos e restando apenas 1
 
-let ArrayFiltrada = nomes.filter((Element,index, Array)=> Array.indexOf(Element) == index);
+//O index off é um outro método na qual está sendo utilizado para encontrar os elementos identicos ao index 
+
+let ArrayFiltrada = nomes.filter((Element, index, Array) => Array.indexOf(Element) == index);
+
+//Exibir a nossa nova Array com os nomes filtrados
 
 console.log(ArrayFiltrada);
 
+//Saída esperada: [ 'Ana', 'Pedro', 'Paulo', 'Noah' ]
+
 //Exemplo 2 -------------------------------------------
+
+//Inseri alguns núemros 
+
+let numeros = [1, 18, 30, 60, 1000, 5000, 208, 603, 0];
+
+// Desejo que filtre apenas os números maiores que 100
+
+let NovaArray = numeros.filter(numeros => numeros > 100);
+
+console.log(NovaArray);
+
+
+
