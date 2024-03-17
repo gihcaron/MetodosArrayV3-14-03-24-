cfde1715fcf10b035dda9d8f921aef2bd1d1b0c6
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 21:50:10 -0300

    Metodo_Array.prototype.pop - Exemplo 1

---
+++ MetodoArray.prototype.pop().js
@@ -0,0 +1,18 @@
+// Método 7 - Array.prototype.pop()
+
+// Utilizade: O Metodo Pop remove o último elemento de um array e retorna esse elemento.
+
+// Sitaxe: pop()
+
+// Exemplo 1 -----------------------------
+
+// Definir constante dos Melhores chocolates  
+const MelhoresChocolates = ["Batom Garoto", "Caribe", "Alpino", "Prestigio", "Amandita"];
+
+// Remover o último elemento (Amandita)
+MelhoresChocolates.pop();
+
+// Exibir novamente a constante sem o último elemento
+console.log(MelhoresChocolates);
+
+// Exemplo 2 ------------------------------
