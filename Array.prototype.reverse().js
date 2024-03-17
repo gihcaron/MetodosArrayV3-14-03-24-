f329c3bf7dd250800b3164e35fb28f034489ead2
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 11:37:58 -0300

    Metodo_Reverse - Exemplo 2

--- Array.prototype.reverse().js
+++ Array.prototype.reverse().js
@@ -7,15 +7,34 @@
 
 //Exemplo 1 ---------------------------
 
+// Determine a Array frase
+
 let frase = "Amo codar : ) "
 
 //Armaenar em uma outra variável: --
-// O resultado igualar, divir a uma string vazia, aplicar o método reverse ao Array e depois juntar novamente os elementos dentro da String (com o Metodo Join) 
+// O igualar o resultado, divir a uma string vazia, aplicar o método reverse ao Array e depois juntar novamente os elementos dentro da String (com o Metodo Join) 
 
 let resultado = frase.split('').reverse().join('');
 
+//Exibir o resultado dessa nova variável 
+
 console.log(resultado); 
 
+//Saída esperada: ) : radoc omA 
 
 
 //Exemplo 2 ------------------------------
+
+// Determine a Array nome
+
+let nome = ["Maria", "José"];
+
+//Armazenar outra variável: --
+//O igualar o resultado ao metodo reverse (a frase invertida)
+
+let res = nome.reverse('');
+
+//Exibir que o resultado é a variável "res"
+console.log('Resultado', res);
+
+//Saída esperada: Resultado [ 'José', 'Maria' ]
