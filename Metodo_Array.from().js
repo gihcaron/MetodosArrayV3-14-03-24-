6d4ee22ee8543be6d476acae872461fcc93f3908
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-17 10:01:56 -0300

    Metodo_Array.prototype.from - Exemplo 1

---
+++ Metodo_Array.from().js
@@ -0,0 +1,18 @@
+// Metodo 9 - Array.from()
+
+//Utilidade: Transforma um objeto iterável ou semelhante a um array em um Array
+
+//Sintaxe: Array.from()
+
+//Exemplo 1 ----------------------------------------------
+
+// Declare a constante desejada a ser transformada em Array
+const nome = 'Giovanna';
+
+//Exiba o nome transformado em uma Array
+console.log(Array.from(nome));
+
+//Saída Esperada: [ 'G', 'i', 'o', 'v', 'a,' 'n', 'n', 'a' ] 
+
+
+// Exemplo 2 ----------------------------------------------
